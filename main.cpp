0fc96624e4d2ceab1152bdf4ad50723c4bbb0104
MXSS <mxsshere@gmail.com>
2017-11-08 12:54:05 +0300

    task 9_1 & 9_3

--- main.cpp
+++ main.cpp
@@ -1,118 +1,80 @@
 #include <iostream>
 using namespace std;
 
-int binpow(int a, int n) {
-    int res = 1;
-    while(n) {
-        if (n & 1) {
-            res *= a;
-            --n;
+void task9_1() {
+    cout << "Type a:" << endl;
+    int a = 0;
+    scanf("%d", &a);
+    cout << "Type b:" << endl;
+    int b = 0;
+    scanf("%d", &b);
+
+    int progress = 0;
+
+    int amount = 0;
+
+    while (progress < a) {
+        if (progress + b < a) {
+            progress += b;
+            amount++;
         } else {
-            a *= a;
-            n >>= 1;
+            break;
         }
     }
-    return res;
+
+    int freeSpace = a - progress;
+
+    cout << "amount: " << amount << endl;
+    cout << "free space: " << freeSpace << endl;
 }
 
-void Task8_1(int a, int b) {
-    int sum = 0;
-    int numAmount = 0;
-
-    int swap = 0;
-
-    if (a < b) {
-        swap = a;
-        a = b;
-        b = swap;
+int pow(int num, int power) {
+    int curPow = 1;
+    while (curPow < power) {
+        num = num * num;
+        curPow++;
     }
-
-    while (a-1 > b) {
-        a--;
-        cout << a << endl;
-
-        numAmount++;
-        sum+=a;
-    }
-
-    cout << "Amount: " << numAmount << endl;
-    cout << "Sum: " << sum << endl;
+    return num;
 }
 
-void Task8_4(int n) {
-    int result = 0;
+void task9_3() {
+    cout << "Type n:" << endl;
+    int n = 0;
+    int k = 0;
 
-    int s = 0;
-    for (int i = 0; i < n; i++) {
-        s = s + n;
-        cout << "s " << s << endl;
-    }
-    cout << "result: " << result;
-}
+    scanf("%d", &n);
 
-void Task8_5(int a, int n) {
-    int i = 1;
-    int s = 1;
-    for (i = 1; i <= n; i++) {
-        if (i % 2 == 0) {
-            s = s + binpow(a, n);
-        }
-        if (i % 2 != 0) {
-            s = s - binpow(a, n);
+    if (n > 1) {
+        while (pow(3,k) < n) {
+            k++;
         }
     }
-    cout << "sum =" <<  s << endl;
-}
-
-void Task8_7(int n) {
-    float result = 0;
-    float prev = 0;
-    for (int i = 1; i < n; i++) {
-        float t = i;
-        cout << "n " << t << endl;
-        result = 2 + (1 / t / prev);
-        cout << result << endl;
-        prev = result;
-    }
-    cout << "result: " << result;
+    k--;
+    cout << "result: " << k << endl;
 }
 
 int main() {
+
     setlocale(LC_ALL, "RUS");
 
     int command = 0;
 
     scanf("%d", &command);
 
-    cout << command << endl;
-    int a = 0;
-    int b = 0;
-    int n = 0;
-
     cout << "Task " << command << endl;
+
     switch (command) {
         case 1:
-            cout << "Type A" << endl;
-            scanf("%d", &a);
-            cout << "Type B" << endl;
-            scanf("%d", &b);
-            Task8_1(a,b);
+            task9_1();
             break;
-        case 4:
-            cout << "Type N" << endl;
-            scanf("%d", &n);
-            Task8_4(n);
+        case 3:
+            task9_3();
             break;
         case 5:
-            cout << "Type A" << endl;
-            scanf("%d", &a);
-            cout << "Type N" << endl;
-            scanf("%d", &n);
-            Task8_5(a,n);
+
+            break;
         case 7:
-            cout << "Type N" << endl;
-            scanf("%d", &n);
-            Task8_7(n);
+
             break;
     }
     return 0;
