ae235526961cb5ab63845d9d8dd717398692ccce
evgeny418 <evgeny418@gmail.com>
2017-11-29 12:17:17 +0300

    task 1 task 3

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
+#include "windows.h"
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "RUS");
-    cout << "235";
+void task1() {
+    int mas[10];
+    int a[10][10];
+    int s = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++) {
+            a[i][j] = rand() % 100 - 50;
+            cout << a[i][j]<< " " ;
 
+        }
+        cout << endl;
+    }
+    for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < 10; i++)
+        {
+            s += a[j][i];
+        }
+        mas[j] = s;
+        cout << mas[j] << endl;
+
+    }
+}
+
+void task3() {
+    int result = 0;
+    int a[10][10];
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            a[i][j] = rand() % 100 - 50;
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    };
+    for (int i = 0; i < 10; i++) {
+        result += a[i][i];
+    };
+    cout << "Ответ = " << result;
+}
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    int a;
+    cout << "Введите номер задания: ";
+    cin >> a;
+    switch (a) {
+        case 1: {
+            task1();
+            break;
+        }
+        case 3: {
+            task3();
+            break;
+        }
+        default: {
+            break;
+        }
+    }
     return 0;
 }
