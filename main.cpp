f1c3b629c9c9a26145f61465436f960f6d4353be
evgeny418 <evgeny418@gmail.com>
2017-12-13 13:56:11 +0300

    Вроде как готово, я хз

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include "windows.h"
+#include "math.h"
 using namespace std;
+
+void task1(int *x, int *y) {
+    if (*x<0) {
+        *y = -2.0 / *x;
+    }
+    if ((*x >= 3) && (*x <= 9)) { ;
+        *y = (*x)*(*x)*(*x);
+    }
+    if (*x >= 9) {
+        *y = -*x;
+    }
+    if ((*x >= 0) && (*x<3)) {
+        *y = 26.2;
+    }
+}
 
 void task6(int *a, int *b, int *c) {
     int temp = 0;
+
     while (true) {
         if (*a > *b) {
             temp = *a;
@@ -19,12 +37,38 @@ void task6(int *a, int *b, int *c) {
     }
 }
 
-void task7(int n,float *arr) {
+void task2(int *n, int *sum) {
+    for (int i = 1; i <= *n; i++) {
+        *sum += (i + 1.0) / (2.0*i);
+    }
+}
+
+void task3(int *d, int *k) {
+    *k = ((*k)*10) + *d;
+}
+
+void task4(int *x, int *y) {
+    int z = 0;
+    z = *x;
+    *x = *y;
+    *y = z;
+}
+
+double task5(int *x, int *y) {
+    double average1 = 0;
+    double average2 = 0;
+    average1 = (*x + *y) * 1.0 / 2;
+    average2 = sqrt(*x * (*y));
+    *x = average1;
+    *y = average2;
+}
+
+void task7(int n,float *test) {
     int i = 0;
-    arr[0] = 0.5;
+    test[0] = 0.5;
     for (i = 1; i < n; i++) {
-        arr[i] = (i+1.0)/(2.0 * i);
-        cout << i << ": " << arr[i] << endl;
+        test[i] = (i+1.0)/(2.0 * i);
+        cout << i << ": " << test[i] << endl;
     }
 }
 
@@ -46,17 +90,66 @@ void task8(int n,float *arr, int *minVal, int *maxVal, float *posSum) {
 }
 
 int main() {
+    SetConsoleOutputCP(CP_UTF8);
     int a;
     cout << "Введите номер задания: ";
     cin >> a;
-
     float *arr_pointer;
     int n = 0;
     int i = 0;
     int maxVal = 0;
     int minVal = 0;
     float positiveSum = 0;
+    int x, y, *p_x, *p_y;
     switch (a) {
+        case 1:
+            p_x = &x;
+            p_y = &y;
+            cout << "Введите Х: ";
+            cin >> x;
+            task1(p_x, p_y);
+            cout << "Ответ = " << y;
+            break;
+        case 2:
+            p_x = &x;
+            p_y = &y;
+            cout << "Введите Х: ";
+            cin >> x;
+            task2(p_x, p_y);
+            cout << "Ответ = " << y;
+            break;
+        case 3:
+            p_x = &x;
+            p_y = &y;
+            cout << "Введите D: ";
+            cin >> x;
+            cout << "Введите K: ";
+            cin >> y;
+            task3(p_x, p_y);
+            cout << "Ответ = " << y;
+            break;
+        case 4:
+            p_x = &x;
+            p_y = &y;
+            cout << "Введите x: ";
+            cin >> x;
+            cout << "Введите y: ";
+            cin >> y;
+            task4(p_x, p_y);
+            cout << "X = " << x << "   ";
+            cout << "Y= " << y;
+            break;
+        case 5:
+            p_x = &x;
+            p_y = &y;
+            cout << "Введите x: ";
+            cin >> x;
+            cout << "Введите y: ";
+            cin >> y;
+            task5(p_x, p_y);
+            cout << "Арифметическое = " << x << "   ";
+            cout << "Геометрическое = " << y;
+            break;
         case 6:
             int a;
             int b;
