887ea37314911bb7bd8698f9e1c378133ef8ff59
evgeny418 <evgeny418@gmail.com>
2017-12-27 03:53:17 +0300

    SUC!

--- main.cpp
+++ main.cpp
@@ -1,278 +1,246 @@
-#include <iostream>
 #include "windows.h"
+#include <iostream>
 #include <cmath>
+
 using namespace std;
 
-void task1() {
-    int a = 0;
-    cout << "Введите a: " << endl;
-    scanf("%d", &a);
 
-    int b = 0;
-    cout << "Введите b: " << endl;
-    scanf("%d", &b);
-
-    int c = 0;
-    cout << "Введите c: " << endl;
-    scanf("%d", &c);
-
-    int maxVal = a;
-    char max = 'a';
-
-    if (b > maxVal) {
-        maxVal = b;
-        max = 'b';
+void task_2() {
+    int n = 0;
+    cout << " введите номер" << endl;
+    cin >> n;
+    double mass = 0;
+    cout << " введите массу" << endl;
+    cin >> mass;
+    switch (n) {
+        case 1:
+            cout << mass << " кг " << endl;
+            break;
+        case 2:
+            cout << mass / 1000000 << " кг " << endl;
+            break;
+        case 3:
+            cout << mass / 1000 << " кг " << endl;
+            break;
+        case 4:
+            cout << mass * 1000 << " кг " << endl;
+            break;
+        case 5:
+            cout << mass * 100 << " кг " << endl;
+            break;
     }
-
-    if (c > maxVal) {
-        maxVal = c;
-        max = 'c';
-    }
-
-    cout << "наибольшее число: " <<  max << " (" << maxVal << ")" << endl;
 }
 
-void task2() {
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int temp = 0;
-
-    cout << "Введите a: " << endl;
-    scanf("%d", &a);
-
-    cout << "Введите b: " << endl;
-    scanf("%d", &b);
-
-    cout << "Введите c: " << endl;
-    scanf("%d", &c);
-
-    for (;;) {
-        if (a < b) {
-            temp = a;
-            a = b;
-            b = temp;
-            continue;
-        }
-        if (b < c) {
-            temp = b;
-            b = c;
-            c = temp;
-            continue;
-        }
-        break;
+void task_1() {
+    int day = 0;
+    cout << " введите день" << endl;
+    scanf("%d", &day);
+    switch (day) {
+        case 1:
+            cout << " понедельник  ";
+            break;
+        case 2:
+            cout << " ворник ";
+            break;
+        case 3:
+            cout << " среда ";
+            break;
+        case 4:
+            cout << " сетверг ";
+            break;
+        case 5:
+            cout << " пятница ";
+            break;
+        case 6:
+            cout << " суббота ";
+            break;
+        case 7:
+            cout << " воскресенье  ";
+            break;
+        default: cout << "Ошибка";
+            break;
     }
-
-    cout << "Отсортировано: " << endl;
-    cout << "a : " << a << endl;
-    cout << "b : " << b << endl;
-    cout << "c : " << c << endl;
 }
-
-void task3() {
-    int a = 0;
-    int b = 0;
-    int c = 0;
+void task_3() {
+    int n, t, c;
+    cout <<  "Storona sveta: ";
+    cin >> n;
+    cout << " количество команд ";
+    cin >> c;
+    for (int i = 0; i < c; i++)
+    {
+        cin >> t;
+        switch (t)
+        {
+            case 1:
+            {
+                n = n == 4 ? 1 : n + 1;
+                break;
+            }
+            case -1:
+            {
+                n = n == 1 ? 4 : n - 1;
+                break;
+            }
+            case 2:
+            {
+                if (n == 1 || n == 2) n += 2;
+                else if (n == 3 || n == 4) n -= 2;
+                break;
+            }
+        }
+    }
+    cout << " результат " << n;
+}
+void task_4() {
+    int n = 0;
+    int r = 0;
     int d = 0;
-    int e = 0;
+    int l = 0;
+    int s = 0;
+    cout << " введите номер" << endl;
+    scanf("%d", &n);
+    switch (n) {
+        case 1:
+            cout << "радиус " << endl;
+            scanf("%d", &r);
+            cout << " диаметр " << 2 * r << endl;
+            cout << " длина " << (2 * 3.14 * r) << endl;
+            cout << " площадь " << (3, 14 * r * r) << endl;
 
-    cout << "Введите a: " << endl;
-    scanf("%d", &a);
+            break;
+        case 2:
+            cout << " диаметр " << endl;
+            scanf("%d", &d);
+            cout << " радиус " << d / 2 << endl;
+            cout << " длина " << (3.14 * d) << endl;
+            cout << " площадь " << ((3, 14 * d * d) / 4) << endl;
+            break;
 
-    cout << "Введите b: " << endl;
-    scanf("%d", &b);
-
-    cout << "Введите c: " << endl;
-    scanf("%d", &c);
-
-    cout << "Введите d: " << endl;
-    scanf("%d", &d);
-
-    cout << "Введите e: " << endl;
-    scanf("%d", &e);
-
-    cout << "Числа, меньше чем a (" << a << "), но больше или равны b (" << b << "):" << endl;
-    if (b < a) {
-        cout << "b" << endl;
-    }
-    if (c < a && c >= b) {
-        cout << "c" << endl;
-    }
-    if (d < a && d >= b) {
-        cout << "d" << endl;
-    }
-    if (e < a && e >= b) {
-        cout << "e" << endl;
-    }
-}
-
-void task4() {
-    float aX = 0;
-    float aY = 0;
-
-    float bX = 0;
-    float bY = 0;
-
-    float cX = 0;
-    float cY = 0;
-
-    float distAtoB;
-    float distAtoC;
-
-    cout << "Введите a (X): " << endl;
-    scanf("%f", &aX);
-
-    cout << "Введите a (Y): " << endl;
-    scanf("%f", &aY);
-
-    cout << "Введите b (X): " << endl;
-    scanf("%f", &bX);
-
-    cout << "Введите b (Y): " << endl;
-    scanf("%f", &bY);
-
-    cout << "Введите c (X): " << endl;
-    scanf("%f", &cX);
-
-    cout << "Введите c (Y): " << endl;
-    scanf("%f", &cY);
-
-    distAtoB = (bX - aX) + (bY - aY);
-    if(distAtoB<0) distAtoB*=-1;
-    cout << "рассотяние от a до b: " << distAtoB << endl;
-
-    distAtoC = (cX - aX) + (cY - aY);
-    if(distAtoC<0) distAtoC*=-1;
-    cout << "рассотяние от a до c: " << distAtoC << endl;
-
-    if (distAtoB < distAtoC) {
-        cout << "b ближе к a" << endl;
-        cout << "координаты b: (" << bX << "," << bY <<")" << endl;
-    } else {
-        cout << "c ближе к a" << endl;
-        cout << "координаты c: (" << cX << "," << cY <<")" << endl;
+        case 3:
+            cout << " длина " << endl;
+            scanf("%d", &l);
+            cout << " радиус " << l / (2 * 3.14) << endl;
+            cout << " диаметр " << l / 3.14 << endl;
+            cout << " площадь " << ((l * l) / (4 * 3, 14)) << endl;
+            break;
+        case 4:
+            cout << " площадь " << endl;
+            scanf("%d", &s);
+            cout << " радиус " << sqrt(s / 3.14) << endl;
+            cout << " диаметр " << 2 * sqrt(s / 3.14) << endl;
+            cout << " площадь " << 2 * sqrt(s / 3.14) * 3.14 << endl;
+            break;
     }
 }
 
-void task5() {
-    int X, Y, Y1, Y2, Y3, X1, X2, X3;
-    cout << "Введите кординнаты первой точки:" << endl;
-    cin >> X1;
-    cin >> Y1;
-    X = X1;
-    Y = Y1;
-    cout << "Введите координаты второй точки:" << endl;
-    cin >> X2;
-    cin >> Y2;
-    if(X2>X) X=X2;
-    if(Y2>Y) Y=Y2;
-    cout << "Введите координаты третьей точки:" << endl;
-    cin >> X3;
-    cin >> Y3;
-    if(X3>X) X=X3;
-    if(Y3>Y) Y=Y3;
-    cout << "Координаты четвертой точки: (" << X << ";" << Y << ")";
-}
-
-void task6() {
+void task_5() {
     int x;
-    float y;
-    cout << "Введите X: ";
+    cout << " введите год ";
     cin >> x;
-    if (x<=0) {
-        y=0;
-    } else if (x>=2 && x<8) {
-        y=x*x;
-    } else if (x>=8) {
-        y=x;
-    } else {
-        y=-1.2;
+
+    switch (((x - 1984) % 60) / 12) {
+        case 0:
+            cout <<" зеленый";
+            break;
+        case 1:
+            cout <<" красный ";
+            break;
+        case 2:
+            cout <<" желтый ";
+            break;
+        case 3:
+            cout <<" белый ";
+            break;
+        case 4:
+            cout <<" черный ";
+            break;
     };
-    cout << "Y = " << y;
+    switch (((x - 1984) % 60) % 12) {
+        case 0:
+            cout <<" крыса ";
+            break;
+        case 1:
+            cout <<" корова  ";
+            break;
+        case 2:
+            cout <<" тигр";
+            break;
+        case 3:
+            cout <<"заяц ";
+            break;
+        case 4:
+            cout <<" дракон ";
+            break;
+        case 5:
+            cout <<" змея ";
+            break;
+        case 6:
+            cout <<" лошадь ";
+            break;
+        case 7:
+            cout <<" овца ";
+            break;
+        case 8:
+            cout <<" обезьяна ";
+            break;
+        case 9:
+            cout <<" курица";
+            break;
+        case 10:
+            cout <<" собака ";
+            break;
+        case 11:
+            cout << " свинья ";
+            break;
+    }
 }
 
-void task7() {
-    int x;
-    float y;
-    cout << "Введите X: ";
-    cin >> x;
-    if (x<=0) {
-        y=-2/x;
-    } else if (x>3 && x<9) {
-        y=x*x*x;
-    } else if (x>=9) {
-        y=-x/3;
-    } else {
-        y=26.2;
-    };
-    cout << "Y = " << y;
-}
-
-void task8() {
-    float x, y;
-    cout << "Введите X: ";
-    cin >> x;
-    if (x<=0) {
-        x*=-1;
-        y=-2/x;
-    } else if (x>3 && x<9) {
-        y=sin(x)-1/x;
-    } else if (x>=9) {
-        y=cos(x)+1/x;
-    } else {
-        y=26.2;
-    };
-    cout << "Y = " << y;
-}
-
-void task9() {
-    int Ng;
-    cout << "Введите N: ";
-    cin >> Ng;
-    if (!(Ng%100) && Ng%400) {
-        cout << "В году 365 дней";
-    } else if (!Ng%4) {
-        cout << "В году 365 дней";
-    } else {
-        cout << "В году 366 дней";
-    };
+void task_6() {
+    int m;
+    int d;
+    cout << " введите месяц";
+    cin >> m;
+    cout << " введите месяц";
+    cin >> d;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    int N;
-    cout << "Задание: ";
-    cin >> N;
-    switch(N) {
+    cout << "Введите номер задания" << endl;
+    int task = 0;
+    scanf("%d", &task);
+
+    switch (task) {
         case 1:
-            task1();
+            task_1();
             break;
         case 2:
-            task2();
+            task_2();
             break;
         case 3:
-            task3();
+            task_3();
             break;
         case 4:
-            task4();
+            task_4();
             break;
         case 5:
-            task5();
+            task_5();
             break;
         case 6:
-            task6();
+            task_6();
             break;
-        case 7:
-            task7();
-            break;
-        case 8:
-            task8();
-            break;
-        case 9:
-            task9();
-            break;
+
+        case 0:
+            task_1();
+            task_2();
+            task_3();
+            task_4();
+            task_5();
+            task_6();
         default:
+            cout << "Неверный номер задания" << endl;
             break;
-    };
+    }
     return 0;
 }
