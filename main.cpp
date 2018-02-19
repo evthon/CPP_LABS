dc739a24bbe0feab6d441681cb71b19f9552624c
evgeny418 <evgeny418@gmail.com>
2018-02-20 01:30:47 +0300

    task1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
-#include a 3
+#define file_name "test.txt"
 using namespace std;
 
+void task1() {
+    float element = 0.5;
+    float arr[10];
+    FILE *file;
+    file = fopen(file_name, "wb");
+    for (int i=0; i<10; i++) {
+        arr[i] = element;
+        element*=2*(i+1);
+        fwrite(&arr[i], sizeof(float), 1, file);
+    }
+    fclose(file);
+}
+
+
 int main() {
-    cout << a;
+    int N;
+    cout << "Input the task number: ";
+    cin >> N;
+
+    switch (N) {
+        case 1:
+            task1();
+    }
+
     return 0;
 }
