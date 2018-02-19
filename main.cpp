4840673fbacfdf3b901fbc6c5108703d1eb91e28
evgeny418 <evgeny418@gmail.com>
2018-02-20 01:52:53 +0300

    task3

--- main.cpp
+++ main.cpp
@@ -15,6 +15,50 @@ void task1() {
     fclose(file);
 }
 
+void task2() {
+    float arr[10];
+    FILE *file;
+    file = fopen(file_name, "rb");
+    fread(arr, sizeof(float), 10, file);
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    fclose(file);
+}
+
+void task3() {
+    float arr[10];
+    FILE *file;
+    int i = 0;
+    file = fopen(file_name, "rb");
+    while(!feof(file)) {
+        fread(&arr[i], sizeof(float), 1, file);
+        i++;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    fclose(file);
+}
+
+void task4() {
+    float arr[10];
+    FILE *file;
+    FILE *result_file;
+    int i = 0;
+    file = fopen(file_name, "rb");
+    result_file = fopen("result.txt", "w");
+    while(!feof(file)) {
+        fread(&arr[i], sizeof(float), 1, file);
+        printf_s(result_file, )
+        i++;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    fclose(file);
+}
+
 
 int main() {
     int N;
@@ -24,6 +68,13 @@ int main() {
     switch (N) {
         case 1:
             task1();
+            break;
+        case 2:
+            task2();
+            break;
+        case 3:
+            task3();
+            break;
     }
 
     return 0;
