4a9440d08e7f2dce1d830ce76afd3e139a673b76
evgeny418 <evgeny418@gmail.com>
2018-02-20 19:56:03 +0300

    Вроде готово, с последним не особо понятно

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #define file_name "test.txt"
+#include <cmath>
+#define kub(a) sqrt(a*a*a)
 using namespace std;
 
 void task1() {
@@ -51,15 +53,41 @@ void task4() {
     while(!feof(file)) {
         fread(&arr[i], sizeof(float), 1, file);
         cout << arr[i] << " ";
-        int a = 5;
-        fprintf_s(result_file, "%f\n" , arr[i]);
+        fprintf_s(result_file, "%.3f\n" , arr[i]);
         i++;
     }
-//    for (int i = 0; i < 10; i++) {
-//        cout << arr[i] << " ";
-//    }
     fclose(file);
     fclose(result_file);
+}
+
+void task5() {
+    int number;
+    float entry;
+    FILE *file;
+    file = fopen(file_name, "rb+");
+    cout << "Input the elemint number in array: "; cin >> number;
+    fseek(file, sizeof(float)*number, SEEK_SET);
+    cout << "Input element: "; cin >> entry;
+    fwrite(&entry, sizeof(float), 1, file);
+    fclose(file);
+    task3();
+}
+
+void task6() {
+    FILE *file;
+    file = fopen(file_name, "rb");
+    float a;
+    for (int i = 0; i < 5; i++) {
+        fseek(file, (sizeof(float)*i*-1), SEEK_END);
+        fread(&a, sizeof(float), 1, file);
+        cout << a << " ";
+    }
+    fclose(file);
+}
+
+void task7() {
+    int a = kub(3);
+    cout << a;
 }
 
 
@@ -81,6 +109,17 @@ int main() {
         case 4:
             task4();
             break;
+        case 5:
+            task5();
+            break;
+        case 6:
+            task6();
+            break;
+        case 7:
+            task7();
+            break;
+        case 8:
+           cout << kub(2);
     }
 
     return 0;
