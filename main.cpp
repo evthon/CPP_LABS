5abcc205ce05244bdb93475e44ddf64535c5e987
evthon.dev <evthon.dev@gmail.com>
2018-04-18 23:23:48 +0300

    Все сделано

--- main.cpp
+++ main.cpp
@@ -4,35 +4,58 @@ using namespace std;
 class complx {
 
     private:
-    int i;
-    int j;
+
+    friend void sub(complx&, complx);
 
     public:
-    complx (int i, int j) {
-        input(i, j);
+    int i;
+    int j;
+    complx () {
+        i = 1;
+        j = 2;
     }
 
-    void input (int i, int j) {
+    void input () {
         cout << "Input number: " << endl;
         cin >> i;
         cin >> j;
     }
 
-    void output (int i, int j) {
-        cout << i << " + " << j << "i";
+    void output () {
+        cout << i << " + " << j << "i" << endl;
+    }
+
+    void add (complx obj) {
+        i += obj.i;
+        j += obj.j;
     }
 
     ~complx () {
-        cout << "\nDestructor run's";
     }
 };
 
+void sub(complx& obj, complx obj2) {
+    obj.i -= obj2.i;
+    obj.j -= obj2.j;
+}
+
+complx task5 (complx obj, complx obj2) {
+
+    complx obj3;
+    obj3.i = obj.i + obj2.i;
+    obj3.j = obj.j + obj2.j;
+
+    return obj3;
+}
+
 int main() {
-    int a, b;
 
-    complx cm(a, b);
-    cm.output(a, b);
-
-
+    complx obj;
+    obj.input();
+    complx obj2;
+    obj.add(obj2);
+    obj.output();
+    sub(obj ,obj2);
+    obj.output();
     return 0;
 }
