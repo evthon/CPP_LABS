bf6eb151b728fdefea41427bf76b594c88c9603b
evthon.dev <evthon.dev@gmail.com>
2018-04-18 14:04:30 +0300

    Только начал

--- main.cpp
+++ main.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main() {
 
+    class complex{
+        complex () {
+            cin >> i;
+            cin >> j;
+        }
+        public:
+        int i, j;
+        void input (i, j) {
+            cout << "Input number: " << endl;
+            cin >> i;
+            cin >> j;
+        }
+        void output () {
+            cout << i << " + " << j << "i";
+        }
+    };
+
     return 0;
 }
